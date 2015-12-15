e39388d70c8cc2b28ac1a3441e6cb1c10b0ac847
andpayne <andpayne@cisco.com>
2015-12-15 15:47:08 -0800

    Added another replacement to cover the Chrome debugging facility. Excluded extraneous node_modules locations that greatly slow down the replacement process.

--- fix_react_native_port.sh
+++ fix_react_native_port.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 # Fixes this Xcode error:
 # Connection to localhost port 8081 [tcp/sunproxyadmin] succeeded! Port 8081 already in use, packager is either not running or not running correctly
-#
 # Run from the root directory of the project
 
 # 8082 unless a replacement port number is specified
 newport=${1:-8082}
 
-find . -type f -print0 | LC_ALL=C xargs -0 sed -i "" "s/:8081/:$newport/g"
-find . -type f -print0 | LC_ALL=C xargs -0 sed -i "" "s/default: 8081/default: $newport/g"
-find . -type f -print0 | LC_ALL=C xargs -0 sed -i "" "s/localhost 8081/localhost $newport/g"
+find . ! -wholename '*node_modules/react-native/node_modules*' -type f -print0 | LC_ALL=C xargs -0 sed -i "" "s/:8081/:$newport/g"
+find . ! -wholename '*node_modules/react-native/node_modules*' -type f -print0 | LC_ALL=C xargs -0 sed -i "" "s/default: 8081/default: $newport/g"
+find . ! -wholename '*node_modules/react-native/node_modules*' -type f -print0 | LC_ALL=C xargs -0 sed -i "" "s/localhost 8081/localhost $newport/g"
+find . ! -wholename '*node_modules/react-native/node_modules*' -type f -print0 | LC_ALL=C xargs -0 sed -i "" "s/\?\: 8081;/\?\: $newport;/g"
 
 
